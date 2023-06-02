d3dc4705151d49e926dd7afb9edd5844fb79a8f3
priyanshusingh <9415762705ps@gmail.com>
2023-06-02 22:49:26 +0530

    .

---
+++ 268. Missing Number/Solutions.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int m = nums[nums.length-1];
+        int l = nums.length;
+        int sum = 0;
+        int sum2 = 0;
+        for(int i=1; i<=l; i++){
+            sum=sum+i;
+        }
+        for(int i=0; i<l; i++){
+            sum2 = sum2+nums[i];
+        }
+
+        int res = sum-sum2;
+        return res;
+    }
+}
