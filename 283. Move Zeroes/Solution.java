015b48ad34f163cfb9431629825c2c0e0d896113
priyanshusingh <9415762705ps@gmail.com>
2023-06-04 00:36:35 +0530

    .

---
+++ 283. Move Zeroes/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        for(int i=0; i<nums.length; i++){
+            for(int j=0; j<nums.length-1; j++){
+                if(nums[j]==0){
+               int temp = nums[j];
+                nums[j]=nums[j+1];
+                nums[j+1]=temp;
+            }
+            }
+            
+        }
+    }
+}
