e75b42fb348db5dcb043591efb50e172e4ac6180
priyanshusingh <9415762705ps@gmail.com>
2023-06-30 22:40:26 +0530

    .

---
+++ 1346. Check If N and Its Double Exist/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length; j++){
+                if(i==j) 
+                continue;
+
+                if(arr[i]*2==arr[j])
+                 return true;
+            }
+        }
+
+        return false;
+    }
+}
